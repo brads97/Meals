36146d313444f0d97c60ea3a8494024f42770f32
unknown <brads97>
2023-04-22 15:03:58 +0100

    Updated Code

--- src/main/java/BS/Meals/Controller/MealsController.java
+++ src/main/java/BS/Meals/Controller/MealsController.java
@@ -3,6 +3,7 @@ package BS.Meals.Controller;
 import BS.Meals.Manager.MealsManager;
 import BS.Meals.Service.MealsService;
 import BS.Meals.User.Meals;
+import BS.Meals.User.UserRequest;
 import ch.qos.logback.core.model.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class MealsController {
         this.mealsService = mealsService;
     }
 
-    private MealsService mealsService;
+    private final MealsService mealsService;
 
     @GetMapping("/all-meals")
     public List<Meals> getAllMeals() {
@@ -31,10 +32,8 @@ public class MealsController {
     }
 
     @PostMapping
-    public List<Meals> postMealPlan(@RequestBody Meals request) {
+    public List<Meals> postMealPlan(@RequestBody UserRequest request) {
         return mealsService.postMealPlan(request.getBreakfastFoodType(),request.getLunchFoodType(),request.getDinnerFoodType1(),request.getDinnerFoodType2());
     }
-
-
 }
 
--- src/main/java/BS/Meals/User/Meals.java
+++ src/main/java/BS/Meals/User/Meals.java
@@ -10,10 +10,7 @@ public class Meals {
     private String gramsCarbs;
     private String gramsFat;
     private String timeToCook;
-    private String breakfastFoodType;
-    private String lunchFoodType;
-    private String dinnerFoodType1;
-    private String dinnerFoodType2;
+
 
     public int getId() {
         return id;
@@ -79,35 +76,5 @@ public class Meals {
         this.timeToCook = timeToCook;
     }
 
-    public String getBreakfastFoodType() {
-        return breakfastFoodType;
-    }
-
-    public void setBreakfastFoodType(String breakfastFoodType) {
-        this.breakfastFoodType = breakfastFoodType;
-    }
-    public String getLunchFoodType() {
-        return lunchFoodType;
-    }
-
-    public void setLunchFoodType(String lunchFoodType) {
-        this.lunchFoodType = lunchFoodType;
-    }
-
-    public String getDinnerFoodType1() {
-        return dinnerFoodType1;
-    }
-
-    public void setDinnerFoodType1(String dinnerFoodType1) {
-        this.dinnerFoodType1 = dinnerFoodType1;
-    }
-
-    public String getDinnerFoodType2() {
-        return dinnerFoodType2;
-    }
-
-    public void setDinnerFoodType2(String dinnerFoodType2) {
-        this.dinnerFoodType2 = dinnerFoodType2;
-    }
 }
 
